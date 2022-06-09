be55b0c3481b9edd7ec6ed161d164375eee8ea04
Nawaz Danish <nawazdanish1996@gmail.com>
2022-06-09 12:56:32 +0530

    added git 2

---
+++ git2.js
@@ -0,0 +1,47 @@
+// one file to push after changes in file
+/*
+git add index.html
+git commit -m "changed few lines"
+git push origin master
+git status
+*/
+
+
+// To login in git
+/*
+git config --global user.name "Nawaz Danish"
+git config --global user.email "nawazdanish1996@gmail.com"
+*/
+
+// To Know your name and email
+/*
+git config --global user.name
+git config --global user.email
+*/
+
+// to see list
+/*
+git config --list
+*/
+
+// to check status
+/*
+git status
+*/
+
+// to know all commits
+/*
+git log
+*/
+
+// got snapshot in git
+/*
+git commit -m "initial commit"
+git status
+*/
+
+// which file name you have mentioned in .gitignore file.
+// it always will be ignored by git after changes in file
+
+
+
