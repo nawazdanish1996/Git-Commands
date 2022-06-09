5bff7366ed00ab2741b4b78ef62eb58073d15a32
Nawaz Danish <nawazdanish1996@gmail.com>
2022-06-09 12:31:29 +0530

    initial commit

---
+++ git1.js
@@ -0,0 +1,28 @@
+/* 
+Make repository first from the Github website
+
+git init
+git status
+git add . (dot)
+git commit -m "initial commit"
+git status
+git remote add origin sshLink
+git push origin master
+
+*/
+
+
+
+// After modification in file
+/*
+git status
+git add --a
+git commit -m "initial commit"
+git status
+git push origin master
+*/
+
+// To create master branch
+/*
+git checkout -b master
+*/
